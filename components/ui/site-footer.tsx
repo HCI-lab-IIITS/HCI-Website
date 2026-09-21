627360e12732ca393ba213c51fddabac2c5c6d5c
Chaitanya Anand <thechaitanyaanand@gmail.com>
2026-09-21 19:27:47 +0530

    Reapply "feat(footer): add Wearable Computing & EgoScore link to research domains"
    This reverts commit 4a9320f3abc144c644efc1e4bf3ef2210d7ea57e.

--- components/ui/site-footer.tsx
+++ components/ui/site-footer.tsx
@@ -45,6 +45,11 @@ export function SiteFooter() {
             </h4>
             <ul className="space-y-2.5 text-xs sm:text-sm text-slate-400 font-light">
               <li>
+                <Link href="/projects/egoscore" className="hover:text-white transition-colors">
+                  Wearable Computing & EgoScore
+                </Link>
+              </li>
+              <li>
                 <Link href="/projects/vr-badminton" className="hover:text-white transition-colors">
                   Spatial Computing & VR Coaching
                 </Link>
